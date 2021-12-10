399cb7d89f1df33706bf0abd59fb7a704d696b96
Akif <dereakif@gmail.com>
2021-12-11 00:26:58 +0300

    create and import: TodoForm, TodoList

---
+++ src/components/TodoForm/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const TodoForm = (props: Props) => {
+  return <div>TodoForm</div>;
+};
+
+export default TodoForm;
---
+++ src/components/TodoList/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const TodoList = (props: Props) => {
+  return <div>TodoList</div>;
+};
+
+export default TodoList;
