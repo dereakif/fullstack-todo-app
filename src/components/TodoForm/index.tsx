801193d60cafbdfc107684e19e6f4143635494d4
Akif <dereakif@gmail.com>
2021-12-11 01:03:27 +0300

    create: form and TodoInput type

--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { TodoInput } from "../../interfaces/todo.interfaces";
 
 interface Props {}
 
+const initialTodoInput = {
+  title: "",
+  description: "",
+  isCompleted: false,
+};
 const TodoForm = (props: Props) => {
-  return <div>TodoForm</div>;
+  const [todos, setTodos] = useState<TodoInput[]>([]);
+  const [input, setInput] = useState<TodoInput>(initialTodoInput);
+  return (
+    <div>
+      <form>
+        <input placeholder="todo title" name="title" type="text" />
+        <input placeholder="todo description" name="description" type="text" />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default TodoForm;
---
+++ src/interfaces/todo.interfaces.ts
@@ -0,0 +1,5 @@
+export type TodoInput = {
+  title: String;
+  description: String;
+  isCompleted: Boolean;
+};
