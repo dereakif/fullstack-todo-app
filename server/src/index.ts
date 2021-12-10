3a405014d14d997da954a762af50dfc6009ae734
Akif <dereakif@gmail.com>
2021-12-10 23:51:31 +0300

    implement: todoRouter

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { json } from "body-parser";
+import { todoRouter } from "./routes/todo";
 
 const app = express();
 
 app.use(json());
+app.use(todoRouter);
 
 app.listen(3001, () => {
   console.log("server is running on port 3001");
