fed1dbbee606324f0bcb2141e93071af7046cf14
Akif <dereakif@gmail.com>
2021-12-11 02:00:45 +0300

    remove: todos state, pass it to childs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import { TodoInput } from "./interfaces/todo.interfaces";
 
 function App() {
+  const [todos, setTodos] = useState<TodoInput[]>([]);
   return (
     <div className="App">
-      <TodoForm />
-      <TodoList />
+      <TodoForm todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
