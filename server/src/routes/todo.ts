db7c47c5d058abc59adc5700d76769052bd3efd6
Akif <dereakif@gmail.com>
2021-12-11 00:13:00 +0300

    update: routes

--- server/src/routes/todo.ts
+++ server/src/routes/todo.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response } from "express";
+import { Todo } from "../models/todo";
 
 const router = express.Router();
 
-router.get("/api/todo", (req: Request, res: Response) => {
-  return res.send("todo");
+router.get("/api/todo", async (req: Request, res: Response) => {
+  const todos = await Todo.find({});
+  return res.status(201).send(todos);
 });
 
-router.post("/api/todo", (req: Request, res: Response) => {
-  return res.send("new todo created");
+router.post("/api/todo", async (req: Request, res: Response) => {
+  const { title, description, isCompleted } = req.body;
+  const todo = Todo.build({ title, description, isCompleted });
+  await todo.save();
+  return res.status(201).send(todo);
 });
 
 export { router as todoRouter };
