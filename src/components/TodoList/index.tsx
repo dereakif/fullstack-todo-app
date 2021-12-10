910a678815f02675448694bdc49b24deb5066be1
Akif <dereakif@gmail.com>
2021-12-11 02:01:39 +0300

    set: default render for Todolist

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { SetStateAction } from "react";
+import { TodoInput } from "../../interfaces/todo.interfaces";
 
-interface Props {}
+interface Props {
+  todos: TodoInput[];
+  setTodos: React.Dispatch<SetStateAction<TodoInput[]>>;
+}
 
 const TodoList = (props: Props) => {
+  const { todos, setTodos } = props;
+  if (todos.length === 0) {
+    return <div>There is no list to display...</div>;
+  }
   return <div>TodoList</div>;
 };
 
