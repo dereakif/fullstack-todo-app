3fc2f59b5c10d033b22c20c05b9fc62bdad384b9
Akif <dereakif@gmail.com>
2021-12-11 00:11:41 +0300

    create: Model and Document interface, Schema

---
+++ server/src/models/todo.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+interface ITodo {
+  title: string;
+  description: string;
+  isCompleted: boolean;
+}
+
+interface todoModelInterface extends mongoose.Model<TodoDoc> {
+  build(attr: ITodo): TodoDoc;
+}
+
+interface TodoDoc extends mongoose.Document {
+  title: string;
+  description: string;
+  isCompleted: boolean;
+}
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  isCompleted: { type: Boolean, required: true },
+});
+
+todoSchema.statics.build = (attr: ITodo) => {
+  return new Todo(attr);
+};
+const Todo = mongoose.model<TodoDoc, todoModelInterface>("Todo", todoSchema);
+
+export { Todo };
