5dbf664bbc583b827d828cbf0feeb30048fafb63
Akif <dereakif@gmail.com>
2021-12-11 02:12:17 +0300

    implement: fetch logic

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,17 +1,42 @@
-import React, { SetStateAction } from "react";
-import { TodoInput } from "../../interfaces/todo.interfaces";
+import axios from "axios";
+import React, { SetStateAction, useEffect } from "react";
+import { Todo } from "../../interfaces/todo.interfaces";
 
 interface Props {
-  todos: TodoInput[];
-  setTodos: React.Dispatch<SetStateAction<TodoInput[]>>;
+  todos: Todo[];
+  setTodos: React.Dispatch<SetStateAction<Todo[]>>;
 }
 
 const TodoList = (props: Props) => {
   const { todos, setTodos } = props;
+  const getTodos = async (): Promise<void> => {
+    try {
+      const response = await axios.get("http://localhost:3001/api/todo");
+      if (response.status === 200) {
+        setTodos(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   if (todos.length === 0) {
     return <div>There is no list to display...</div>;
   }
-  return <div>TodoList</div>;
+  return (
+    <div>
+      {todos.map((todo) => (
+        <div key={todo._id}>
+          <p style={{ fontWeight: "bold" }}>{todo.title}</p>
+          <p>{todo.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
