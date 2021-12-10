9b55c2419fd5a1866e0a9f7b1c69ce62f59bbef6
Akif <dereakif@gmail.com>
2021-12-11 02:02:06 +0300

    implement: onChange logic to form

--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, {
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import { TodoInput } from "../../interfaces/todo.interfaces";
 
-interface Props {}
+interface Props {
+  todos: TodoInput[];
+  setTodos: React.Dispatch<SetStateAction<TodoInput[]>>;
+}
 
 const initialTodoInput = {
   title: "",
@@ -9,13 +18,35 @@ const initialTodoInput = {
   isCompleted: false,
 };
 const TodoForm = (props: Props) => {
-  const [todos, setTodos] = useState<TodoInput[]>([]);
+  const { todos, setTodos } = props;
   const [input, setInput] = useState<TodoInput>(initialTodoInput);
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { value, name } = event.target;
+    setInput((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleCheckBox = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { checked } = event.target;
+    setInput((prev) => ({ ...prev, isCompleted: checked }));
+  };
+
   return (
     <div>
       <form>
-        <input placeholder="todo title" name="title" type="text" />
-        <input placeholder="todo description" name="description" type="text" />
+        <input
+          placeholder="todo title"
+          name="title"
+          type="text"
+          onChange={handleOnChange}
+        />
+        <input
+          placeholder="todo description"
+          name="description"
+          type="text"
+          onChange={handleOnChange}
+        />
+        <input name="isCompleted" type="checkbox" onChange={handleCheckBox} />
         <button type="submit">submit</button>
       </form>
     </div>
