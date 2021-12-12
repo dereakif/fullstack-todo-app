8f1dd0bdbed7bbf4e82f59d01e83c9e61aebed33
Akif <dereakif@gmail.com>
2021-12-12 21:56:08 +0300

    implement: onChangeFunction, StyledForm, formGroup

--- src/components/TodoForm/InputForm.tsx
+++ src/components/TodoForm/InputForm.tsx
@@ -1,13 +1,18 @@
 import React, { ChangeEvent, FormEvent } from "react";
-import { isError, Todo, TodoInput } from "../../interfaces/todo.interfaces";
-import { StyledButton, StyledInput, StyledTextArea } from "./styles";
+import {
+  onChangeFunction,
+  onSubmitFunction,
+  Todo,
+  TodoInput,
+} from "../../interfaces/todo.interfaces";
+import { StyledButton, StyledForm, StyledSubmitContainer } from "./styles";
 
 interface Props {
-  handleOnChange: (event: ChangeEvent<HTMLInputElement>) => void;
-  handleCheckBox: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleOnChange: onChangeFunction;
+  handleCheckBox: onChangeFunction;
   input: TodoInput | Todo;
-  handleSubmitCreate?: (e: FormEvent<HTMLFormElement>) => isError;
-  handleSubmitEdit?: (e: FormEvent<HTMLFormElement>) => isError;
+  handleSubmitCreate?: onSubmitFunction;
+  handleSubmitEdit?: onSubmitFunction;
 }
 
 const InputForm = (props: Props) => {
@@ -20,31 +25,46 @@ const InputForm = (props: Props) => {
   } = props;
 
   return (
-    <form onSubmit={handleSubmitEdit || handleSubmitCreate}>
-      <StyledInput
-        placeholder="todo title"
-        name="title"
-        type="text"
-        value={input.title}
-        onChange={handleOnChange}
-      />
-      <StyledTextArea
-        placeholder="todo description"
-        name="description"
-        type="text"
-        value={input.description}
-        onChange={handleOnChange}
-      />
-      <input
-        name="isCompleted"
-        type="checkbox"
-        checked={input.isCompleted}
-        onChange={handleCheckBox}
-      />
-      <StyledButton type="submit">
-        {handleSubmitEdit ? "done" : "submit"}
-      </StyledButton>
-    </form>
+    <StyledForm onSubmit={handleSubmitEdit || handleSubmitCreate}>
+      <StyledForm.Group className="mb-3" controlId="title">
+        <StyledForm.Label>Todo Title</StyledForm.Label>
+        <StyledForm.Control
+          type="text"
+          placeholder="Enter title"
+          name="title"
+          value={input.title}
+          onChange={handleOnChange}
+        />
+      </StyledForm.Group>
+
+      <StyledForm.Group className="mb-3" controlId="description">
+        <StyledForm.Label>Description</StyledForm.Label>
+        <StyledForm.Control
+          type="text"
+          as="textarea"
+          name="description"
+          placeholder="Enter description"
+          value={input.description}
+          onChange={handleOnChange}
+        />
+      </StyledForm.Group>
+
+      <StyledForm.Group className="mb-3" controlId="isCompleted">
+        <StyledSubmitContainer>
+          <StyledForm.Check
+            checked={input.isCompleted}
+            onChange={handleCheckBox}
+            type="checkbox"
+            name="isCompleted"
+            label="Completed"
+          />
+
+          <StyledButton type="submit">
+            {handleSubmitEdit ? "done" : "submit"}
+          </StyledButton>
+        </StyledSubmitContainer>
+      </StyledForm.Group>
+    </StyledForm>
   );
 };
 
