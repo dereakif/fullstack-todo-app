190ff385b2e818d8dadbc080cf45c9af59d7a555
Akif <dereakif@gmail.com>
2021-12-14 02:20:35 +0300

    add: box-shadow to TitleContainer

--- src/components/TodoList/styles.ts
+++ src/components/TodoList/styles.ts
@@ -49,6 +49,7 @@ export const TitleContainer = styled(Col)`
   &:hover {
     transform: scale(1.03);
   }
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
 `;
 
 export const TodoContainer = styled(Row)`
