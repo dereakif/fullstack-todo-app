9ba2e8c22cb09c20f4fabfc63863f74cc4cab4a9
Akif <dereakif@gmail.com>
2021-12-16 22:56:22 +0300

    implement: try catch in routes

--- server/src/routes/todo.ts
+++ server/src/routes/todo.ts
@@ -1,31 +1,62 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Todo } from "../models/todo";
 
 const router = express.Router();
 
-router.get("/api/todo", async (req: Request, res: Response) => {
-  const todos = await Todo.find({});
-  return res.status(201).send(todos);
-});
+router.get(
+  "/api/todo",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const todos = await Todo.find({});
+      return res.status(201).send(todos);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-router.post("/api/todo", async (req: Request, res: Response) => {
-  const { title, description, isCompleted } = req.body;
-  const todo = Todo.build({ title, description, isCompleted });
-  await todo.save();
-  return res.status(201).send(todo);
-});
+router.post(
+  "/api/todo",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { title, description, isCompleted } = req.body;
+    try {
+      const todo = Todo.build({ title, description, isCompleted });
+      await todo.save();
+      return res.status(201).send(todo);
+    } catch (error: unknown) {
+      next(error);
+    }
+  }
+);
 
-router.put("/api/todo", async (req: Request, res: Response) => {
-  const { title, description, isCompleted, _id } = req.body;
-  await Todo.updateOne({ _id }, { $set: { title, description, isCompleted } });
-  const todo = await Todo.findOne({ _id });
-  return res.status(201).send(todo);
-});
+router.put(
+  "/api/todo",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { title, description, isCompleted, _id } = req.body;
+    try {
+      await Todo.updateOne(
+        { _id },
+        { $set: { title, description, isCompleted } }
+      );
+      const todo = await Todo.findOne({ _id });
+      return res.status(201).send(todo);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-router.delete("/api/todo", async (req: Request, res: Response) => {
-  const { _id } = req.body;
-  const result = await Todo.deleteOne({ _id });
-  return res.status(201).send(result);
-});
+router.delete(
+  "/api/todo",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { _id } = req.body;
+    try {
+      const result = await Todo.deleteOne({ _id });
+      return res.status(201).send(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export { router as todoRouter };
