09973e820eac57cfa0b0e1ffdcffcac5bafb505b
Akif <dereakif@gmail.com>
2021-12-16 22:56:38 +0300

    implement: errorHandler

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 import cors from "cors";
 import { json } from "body-parser";
 import { todoRouter } from "./routes/todo";
+import middlewares from "./middlewares";
 
 const app = express();
 
 app.use(json());
 app.use(cors());
 app.use(todoRouter);
+
+app.use(middlewares.errorHandler);
 
 mongoose.connect("mongodb://localhost:27017/todo", () => {
   console.log("connected to database");
