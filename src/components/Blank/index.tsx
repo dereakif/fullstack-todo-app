a45598170a73920961099d4e34b90c172046c559
Akif <dereakif@gmail.com>
2021-12-20 01:36:25 +0300

    create: Blank comp for empty list

---
+++ src/components/Blank/index.tsx
@@ -0,0 +1,7 @@
+import { StyledBlank } from "./styles";
+
+const Blank = () => {
+  return <StyledBlank>There is no list to display...</StyledBlank>;
+};
+
+export default Blank;
---
+++ src/components/Blank/styles.ts
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+export const StyledBlank = styled.div`
+  color: whitesmoke;
+`;
