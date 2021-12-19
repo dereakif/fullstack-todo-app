687e0bc1e95c05df05498ea4e1751a0b7e3eeff3
Akif <dereakif@gmail.com>
2021-12-19 03:09:43 +0300

    implement: Loading for todo list

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -13,6 +13,7 @@ import { StyledTodoList, TitleContainer, TodoContainer } from "./styles";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import { timeAgo } from "../../modules/dates";
+import Loading from "../Loading";
 
 interface Props {
   todoState: TodoState;
@@ -32,12 +33,18 @@ const TodoList = (props: Props) => {
   const [selectedIndex, setSelectedIndex] = useState<number>(-1);
 
   const fetchTodos = useCallback(() => {
-    axios.get("http://localhost:3001/api/todo").then((res) => {
-      const { data } = res;
-      if (data) {
-        setTodoState((prev) => ({ ...prev, data }));
-      }
-    });
+    setTodoState((prev) => ({ ...prev, loading: true }));
+    axios
+      .get("http://localhost:3001/api/todo")
+      .then((res) => {
+        const { data } = res;
+        if (data) {
+          setTodoState((prev) => ({ ...prev, data }));
+        }
+      })
+      .finally(() => {
+        setTodoState((prev) => ({ ...prev, loading: false }));
+      });
   }, [setTodoState]);
 
   useEffect(() => {
@@ -56,8 +63,9 @@ const TodoList = (props: Props) => {
   const deleteTodo = (todoId: string) => {
     const index = todoState.data.findIndex((todo) => todo._id === todoId);
     if (index === -1) {
-      return;
+      return setTodoState((prev) => ({ ...prev, error: "notFound" }));
     }
+    setTodoState((prev) => ({ ...prev, loading: true }));
     axios
       .delete("http://localhost:3001/api/todo", { data: { _id: todoId } })
       .then((res) => {
@@ -66,6 +74,9 @@ const TodoList = (props: Props) => {
           const newTodos = todoState.data.filter((todo) => todo._id !== todoId);
           setTodoState((prev) => ({ ...prev, data: newTodos }));
         }
+      })
+      .finally(() => {
+        setTodoState((prev) => ({ ...prev, loading: false }));
       });
   };
 
@@ -84,24 +95,38 @@ const TodoList = (props: Props) => {
     const trimmiedTitle: string = selectedTodo.title.trim();
     const trimmiedDescription: string = selectedTodo.description.trim();
     if (!trimmiedTitle || !trimmiedDescription) {
-      return { isError: "invalidInput" };
+      return setTodoState((prev) => ({ ...prev, error: "invalidInput" }));
     }
-    axios.put("http://localhost:3001/api/todo", selectedTodo).then((res) => {
-      const { data } = res;
-      if (data) {
-        const newTodos = [...todoState.data];
-        newTodos[selectedIndex] = data;
-        setTodoState((prev) => ({ ...prev, data: newTodos }));
-        setSelectedTodo(initialTodoInput);
-        return { isError: "" };
-      }
-    });
-    return { isError: "noData" };
+    setTodoState((prev) => ({ ...prev, loading: true }));
+    axios
+      .put("http://localhost:3001/api/todo", {
+        ...selectedTodo,
+      })
+      .then((res) => {
+        const { data } = res;
+        if (data) {
+          const newTodos = [...todoState.data];
+          newTodos[selectedIndex] = data;
+          setTodoState((prev) => ({ ...prev, data: newTodos, error: "" }));
+          setSelectedTodo(initialTodoInput);
+        }
+      })
+      .catch((e) => {
+        setTodoState((prev) => ({ ...prev, error: e.response.data.message }));
+      })
+      .finally(() => {
+        setTodoState((prev) => ({ ...prev, loading: false }));
+      });
   };
 
   if (todoState.data?.length === 0) {
     return <div>There is no list to display...</div>;
   }
+
+  if (todoState.loading) {
+    return <Loading />;
+  }
+
   return (
     <StyledTodoList>
       {todoState.data?.map((todo, index) =>
